9934853e03a8da43230f4964d8f4011cda34e487
Fahad Shakeel <mrfahadshakeel1@gmail.com>
2026-08-30 19:44:14 +0500

    Fixed url white page

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
   // HashRouter matches GitHub Pages deploy (.../Local-Van/#/...)
   return (
     <QueryClientProvider client={queryClient}>
-      <HashRouter basename="/Local-Van/">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
